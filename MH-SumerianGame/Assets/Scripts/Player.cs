7b96c0eccef625f7aa4f225e93f97a882e3cf646
Miles <milestonesxh@gmail.com>
2023-03-26 15:00:12 -0400

    SumerianGamedone

--- MH-SumerianGame/Assets/Scripts/Player.cs
+++ MH-SumerianGame/Assets/Scripts/Player.cs
@@ -5,15 +5,31 @@ using TMPro;
 
 public class Player : MonoBehaviour
 {
+    private string playerName;
+    private ulong highestPop;
+    private GameplayManager gameManager;
+
+    public TMP_Text nameText;
+    public TMP_Text yearText;
+    public TMP_Text highestPopText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        nameText.text = "Ruler: " + playerName;
+
+        gameManager = GetComponent<GameplayManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        yearText.text = "Year: " + gameManager.year;
+
+        if (gameManager.population > highestPop)
+        {
+            highestPop = gameManager.population;
+            highestPopText.text = "Highest Population: " + highestPop;
+        }
     }
 }
