e7e8b818fe5d69ba67fa33813c92bf0b18ee39ff
Miles <milestonesxh@gmail.com>
2023-04-16 15:34:48 -0400

    done

--- MH-Space Invaders/Assets/Enemy.cs
+++ MH-Space Invaders/Assets/Enemy.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float speed;
     private Animator animator;
+    static private Vector2 direction;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        direction = Vector2.right;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         animator.SetTrigger("death");
         Destroy(gameObject, 1f);
         Destroy(collision.gameObject);
+    }
+
+    void Update()
+    {
+        transform.Translate(direction * Time.deltaTime * speed);
+
+        if (transform.position.x > 11f)
+        {
+            direction = Vector2.left;
+            MoveDown();
+        }
+        if (transform.position.x < -11f)
+        {
+            direction = Vector2.right;
+            MoveDown();
+        }
+    }
+    private void MoveDown()
+    {
+        foreach (Enemy enemy in FindObjectsOfType(typeof(Enemy)))
+        {
+            enemy.transform.Translate(Vector2.down);
+        }
     }
 }
